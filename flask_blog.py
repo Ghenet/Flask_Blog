ba83e3e663016ae297e1a187ff5a1e57d2836b91
Ghenet Kibrom <ghenetkibrom@Ghenets-MacBook-Pro.local>
2019-03-23 15:08:12 -0700

    validates the signup form and flashes success message

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect 
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -28,16 +28,19 @@ posts =[
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return  render_template('home.html', posts_data=posts)
 
 @app.route('/about')
 def about():
     return render_template('about.html',title='Hey About')    
 
-@app.route('/register')
+@app.route('/register',methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login')
